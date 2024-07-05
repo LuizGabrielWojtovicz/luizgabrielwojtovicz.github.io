2155cdb6ff3497aac8c9c0e542439bf64da23113
Luiz Gabriel Wojtovicz <luigabrielw@gmail.com>
2024-07-05 17:58:52 -0300

    alt_05

--- portifolio/src/index.js
+++ portifolio/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
-import App from './App';
 
 ReactDOM.render(
   <ChakraProvider>
